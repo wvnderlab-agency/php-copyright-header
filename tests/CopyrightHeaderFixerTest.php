65795d7719b08c5517ba90166f08025088ae0fc8
johninamillion <john@millionvisions.de>
2025-08-11 11:48:10 +0200

    🧪 adjust whitespaces and assertations in tests && add a test for overwrites

--- tests/CopyrightHeaderFixerTest.php
+++ tests/CopyrightHeaderFixerTest.php
@@ -5,15 +5,27 @@ namespace WvnderlabAgency\CopyrightHeader\Tests;
 use PhpCsFixer\Tokenizer\Tokens;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
+use ReflectionClass;
 use SplFileInfo;
 use WvnderlabAgency\CopyrightHeader\CopyrightHeaderFixer;
 
 class CopyrightHeaderFixerTest extends TestCase
 {
+    protected function getCopyright(): string
+    {
+        $fixer = new CopyrightHeaderFixer();
+        $reflection = new ReflectionClass($fixer);
+        $method = $reflection->getMethod('getCopyright');
+        $method->setAccessible(true);
+
+        return $method->invoke($fixer);
+    }
+
     #[Test]
     public function adds_header_when_php_tag_present(): void
     {
         $fixer = new CopyrightHeaderFixer();
+        $copyright = $this->getCopyright();
         $input = "<?php\n\n\$foo = 123;\n";
         $tokens = Tokens::fromCode($input);
 
@@ -23,13 +35,14 @@ class CopyrightHeaderFixerTest extends TestCase
         $output = $tokens->generateCode();
 
         $this->assertStringStartsWith("<?php\n\n/**", $output);
-        $this->assertStringContainsString('copyright', $output);
+        $this->assertStringContainsString($copyright, $output);
     }
 
     #[Test]
     public function adds_header_when_php_tag_present_with_spacing(): void
     {
         $fixer = new CopyrightHeaderFixer();
+        $copyright = $this->getCopyright();
         $input = "<?php \n\n\$foo = 123;\n";
         $tokens = Tokens::fromCode($input);
 
@@ -38,14 +51,15 @@ class CopyrightHeaderFixerTest extends TestCase
 
         $output = $tokens->generateCode();
 
-        $this->assertStringStartsWith("<?php \n\n/**", $output);
-        $this->assertStringContainsString('copyright', $output);
+        $this->assertStringStartsWith("<?php\n\n/**", $output);
+        $this->assertStringContainsString($copyright, $output);
     }
 
     #[Test]
     public function adds_header_when_declare_strict_types_present(): void
     {
         $fixer = new CopyrightHeaderFixer();
+        $copyright = $this->getCopyright();
         $input = "<?php declare(strict_types=1);\n\n\$foo = 123;\n";
         $tokens = Tokens::fromCode($input);
 
@@ -55,13 +69,14 @@ class CopyrightHeaderFixerTest extends TestCase
         $output = $tokens->generateCode();
 
         $this->assertStringStartsWith("<?php declare(strict_types=1);\n\n/**", $output);
-        $this->assertStringContainsString('copyright', $output);
+        $this->assertStringContainsString($copyright, $output);
     }
 
     #[Test]
     public function adds_header_when_declare_strict_types_present_with_spacing(): void
     {
         $fixer = new CopyrightHeaderFixer();
+        $copyright = $this->getCopyright();
         $input = "<?php declare(strict_types=1); \n\n\$foo = 123;\n";
         $tokens = Tokens::fromCode($input);
 
@@ -70,8 +85,8 @@ class CopyrightHeaderFixerTest extends TestCase
 
         $output = $tokens->generateCode();
 
-        $this->assertStringStartsWith("<?php declare(strict_types=1); \n\n/**", $output);
-        $this->assertStringContainsString('copyright', $output);
+        $this->assertStringStartsWith("<?php declare(strict_types=1);\n\n/**", $output);
+        $this->assertStringContainsString($copyright, $output);
     }
 
     #[Test]
@@ -99,5 +114,22 @@ class CopyrightHeaderFixerTest extends TestCase
         $fixer->fix(new SplFileInfo('dummy.php'), $tokens);
 
         $this->assertSame($input, $tokens->generateCode());
+    }
+
+    #[Test]
+    public function overwrites_outdated_headers(): void
+    {
+        $fixer = new CopyrightHeaderFixer();
+        $copyright = $this->getCopyright();
+        $outdatedCopyright = str_replace(date('Y'), '2020', $copyright);
+        $input = "<?php\n{$outdatedCopyright}";
+        $tokens = Tokens::fromCode($input);
+
+        // use fix method to apply the fixer, cause applyFix is protected
+        $fixer->fix(new SplFileInfo('test.php'), $tokens);
+
+        $output = $tokens->generateCode();
+
+        $this->assertStringContainsString($copyright, $output);
     }
 }
